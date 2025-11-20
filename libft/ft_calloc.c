b15a31fae91cb1c06a2cd6a519cc73af58cd08bd
42Colins <colinproj@icloud.com>
2025-11-20 07:43:27 -0500

    Really small fix ...

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,13 +6,15 @@
 /*   By: cprojean <cprojean@42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 12:53:52 by cprojean          #+#    #+#             */
-/*   Updated: 2025/09/29 15:38:23 by cprojean         ###   ########.fr       */
+/*   Updated: 2025/11/20 07:42:37 by cprojean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// #define SIZE_MAX 2147483647
+#ifndef SIZE_MAX
+# define SIZE_MAX 2147483647
+#endif
 
 void	*ft_calloc(size_t count, size_t size)
 {
